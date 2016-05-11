071a240a41cfa8934e0b4f0b9f0523204d83b9d1
ErgunK <erguen.kilic@rpn.ch>
2016-05-11 14:43:25 +0200

    OK

--- src/EK/HomepageBundle/Controller/HomepageController.php
+++ src/EK/HomepageBundle/Controller/HomepageController.php
@@ -7,24 +7,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomepageController extends Controller
 {
-	
+
 	public function viewAction($id) {
-		
-		
-		
+
+		//test
+
 		return new Response("Affichage de l'annonce d'id : ".$id);
 	}
-	
-	
+
+
     public function indexAction()
     {
 		// Méthode courte
 		$url = $this->generateUrl('ek_homepage_home');
 
 		//return new Response($url);
-		
+
 		$content = $this->get('templating')->render('EKHomepageBundle:Homepage:index.html.twig', array( 'titre_page' => 'Home'));
-        
+
 		return new Response($content);
 		//return $this->render('EKHomepageBundle:Homepage:index.html.twig');
     }
